7eb7f8390fe535f98a5165ca5785994fa624fc4e
Madhunagula Sandhya <161038831+SandhyaMadhunagula@users.noreply.github.com>
2024-03-25 10:43:55 +0530

    Update assignmet16.py

--- assignmet16.py
+++ assignmet16.py
@@ -17,16 +17,10 @@ def make_amount(rupees_to_make,no_of_five,no_of_one):
         one_needed=rupees_to_make -(5*five_needed)
         print("No. of Five needed :", five_needed)
         print("No. of One needed  :", one_needed)
- 
-        
-
-    # Use the below given print statements to display the output
+  # Use the below given print statements to display the output
     # Also, do not modify them for verification to work
-
-    # print("No. of Five needed :", five_needed)
+# print("No. of Five needed :", five_needed)
      #print("No. of One needed  :", one_needed)
     #print(-1)
-
-
 #Provide different values for rupees_to_make,no_of_five,no_of_one and test your program
 make_amount(28,8,5)
