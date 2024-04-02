d75bbf27505fe673f02701a2d6aedc14abd2230d
Madhunagula Sandhya <161038831+SandhyaMadhunagula@users.noreply.github.com>
2024-04-02 22:09:16 +0530

    Add files via upload

---
+++ formating strings.py
@@ -0,0 +1,55 @@
+#Use " " (a space) to insert a space before positive numbers and a minus sign before negative numbers:
+txt = "The temperature is between {: } and {: } degrees celsius."
+print(txt.format(-3, 7))
+#Use "," to add a comma as a thousand separator:
+txt = "The universe is {:,} years old."
+print(txt.format(13800000000))
+#Use "+" to always indicate if the number is positive or negative:
+txt = "The temperature is between {:+} and {:+} degrees celsius."
+print(txt.format(-3, 7))
+#To demonstrate, we insert the number 8 to set the available space for the value to 8 characters.
+#Use "^" to center-align the value:
+txt = "We have {:^8} chickens."
+print(txt.format(49))
+#To demonstrate, we insert the number 8 to set the available space for the value to 8 characters.
+#Use ">" to right-align the value:
+txt = "We have {:>8} chickens."
+print(txt.format(49))
+#To demonstrate, we insert the number 8 to set the available space for the value to 8 characters.
+#Use "<" to left-align the value:
+txt = "We have {:<8} chickens."
+print(txt.format(49))
+#Use "b" to convert the number into binary format:
+txt = "The binary version of {0} is {0:b}"
+print(txt.format(5))
+#Use "%" to convert the number into a percentage format:
+txt = "You scored {:%}"
+print(txt.format(0.25))
+#Or, without any decimals:
+txt = "You scored {:.0%}"
+print(txt.format(0.25))
+#Use "o" to convert the number into octal format:
+txt = "The octal version of {0} is {0:o}"
+print(txt.format(10))
+#Use "x" to convert the number into Hex format:
+txt = "The Hexadecimal version of {0} is {0:x}"
+print(txt.format(255))
+#Use "F" to convert a number into a fixed point number, but display inf and nan as INF and NAN:
+x = float('inf')
+txt = "The price is {:F} dollars."
+print(txt.format(x))
+#same example, but with a lower case f:
+txt = "The price is {:f} dollars."
+print(txt.format(x))
+#Use "f" to convert a number into a fixed point number, default with 6 decimals, but use a period followed by a number to specify the number of decimals:
+txt = "The price is {:.2f} dollars."
+print(txt.format(45))
+#without the ".2" inside the placeholder, this number will be displayed like this:
+txt = "The price is {:f} dollars."
+print(txt.format(45))
+#Use "_" to add a underscore character as a thousand separator:
+txt = "The universe is {:_} years old."
+print(txt.format(13800000000))
+#To demonstrate, we insert the number 8 to specify the available space for the value.
+#Use "=" to place the plus/minus sign at the left most positiontxt = "The temperature is {:=8} degrees celsius."
+print(txt.format(-5))
