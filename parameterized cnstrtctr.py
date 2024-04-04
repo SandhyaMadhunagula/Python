f186aed7d015598f284672d6bf4cb9b7adcfed25
Madhunagula Sandhya <161038831+SandhyaMadhunagula@users.noreply.github.com>
2024-04-04 19:44:11 +0530

    Add files via upload

---
+++ parameterized cnstrtctr.py
@@ -0,0 +1,13 @@
+#If a constructor takes parameters then it would be called as parameterized constructor.
+
+class Mobile:
+    def __init__(self, brand, price):
+        print("Inside constructor")
+        self.brand = brand
+        self.price = price
+
+mob1=Mobile("Apple", 20000)
+print("Mobile 1 has brand", mob1.brand, "and price", mob1.price)
+
+mob2=Mobile("Samsung",3000)
+print("Mobile 2 has brand", mob2.brand, "and price", mob2.price)
