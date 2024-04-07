042955ec769b47e5019ac4c041085969a6659c7e
Madhunagula Sandhya <161038831+SandhyaMadhunagula@users.noreply.github.com>
2024-04-07 15:03:03 +0530

    SWAP CASE
    Convert all lower case letters in upper case and vice versa.

---
+++ uprlwr.py
@@ -0,0 +1,25 @@
+s="SaNdHya"
+t=""
+for i in s:
+     if  i.isupper():
+        t=i.lower()
+       
+     else:
+          t=i.upper()
+          
+     print(t,end="")
+
+
+
+
+
+
+
+        
+            
+    
+
+
+
+
+        
